9b69d0b6760c6ef90a295b133371aedec194269d
Darshan <darshankumaravelk@gmail.com>
2024-08-10 22:39:04 +0530

    Modified the login page with Angular-Material

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
-
+import {} from '@angular/material'
+import { MatFabButton } from '@angular/material/button';
+import { MatCard, MatCardActions, MatCardContent } from '@angular/material/card';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatCard,
+    MatCardActions,
+    MatCardContent,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatFabButton
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
