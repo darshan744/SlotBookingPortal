de1615824d99bdb1336713a856cf22c4fc3185e0
Darshan <darshankumaravelk@gmail.com>
2024-09-28 21:02:06 +0530

    Successfully changed the height of the input field in table

--- src/app/Admin/admin-home/admin-home.component.ts
+++ src/app/Admin/admin-home/admin-home.component.ts
@@ -1,12 +1,14 @@
 
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, inject, OnDestroy, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatCheckbox } from '@angular/material/checkbox';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatColumnDef, MatHeaderCell, MatHeaderCellDef, MatRowDef, MatTableModule } from '@angular/material/table';
-
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { NgClass } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 export interface stdList{
   No:Number,
@@ -20,12 +22,16 @@ export interface stdList{
   selector: 'app-admin-home',
   standalone: true,
   imports: [MatTableModule,MatColumnDef,MatRowDef,MatHeaderCell,MatHeaderCellDef,MatRowDef,MatRowDef
-    ,MatCheckbox,MatButton,MatInput,MatLabel,MatFormField,FormsModule
+    ,MatCheckbox,MatButton,MatInput,MatLabel,MatFormField,FormsModule,NgClass
   ],
   templateUrl: './admin-home.component.html',
   styleUrl: './admin-home.component.css'
 })
-export class AdminHomeComponent {
+export class AdminHomeComponent{
+  
+  
+  
+
   studentList: stdList[] = [
     { No: 1, Name: 'John Doe', attendance:'', ispresent:false,marks: NaN, remarks: '' },
     { No: 2, Name: 'Jane Smith',attendance:'', ispresent:false, marks: NaN,  remarks: '' },
@@ -42,7 +48,7 @@ export class AdminHomeComponent {
   submitRow(student:stdList){
     console.log("specifc student",student);
   }
-
+  // mobileheader = 'mobileheader';
   attendance(student:stdList){
     student.attendance = student.ispresent?'Present':'Absent'
     console.log(student.attendance);
